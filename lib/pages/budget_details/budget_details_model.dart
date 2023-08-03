29609dff0337016dbd6f67995b4130f87c863d42
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-08-03 19:12:48 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/pages/budget_details/budget_details_model.dart
@@ -0,0 +1,24 @@
+import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_charts.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/pages/budget_d_e_l_e_t_e/budget_d_e_l_e_t_e_widget.dart';
+import '/pages/payment_details/payment_details_widget.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class BudgetDetailsModel extends FlutterFlowModel {
+  /// Initialization and disposal methods.
+
+  void initState(BuildContext context) {}
+
+  void dispose() {}
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+}
